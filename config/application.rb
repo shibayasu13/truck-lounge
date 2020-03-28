90f4d3aeeb40d2316849f8a349cb1d159885a742
shibata yasunobu <shibayasu13@gmail.com>
2020-03-28 11:07:17 +0000

    remove web_console setting from application.rb

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,6 @@ module TruckLounge
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.web_console.whitelisted_ips = '10.0.2.2'
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
 
